f54361bd46561e8aed6edfd09527b209636acde7
Marissa Tamaki <tamaki.marissa@gmail.com>
2016-03-29 23:38:58 -0700

    Added the jQuery code using Velocity.js to make the navbar scroll to the specific section

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // bind click event to all internal page anchors
+    $('a[href*="#"]').on('click', function(e) {
+        // prevent default action and bubbling
+        e.preventDefault();
+        e.stopPropagation();
+        // set target to anchor's "href" attribute
+        var target = $(this).attr('href');
+        // scroll to each target
+        $(target).velocity('scroll', {
+            duration: 1500,
+            easing: 'ease-in-out'
+        });
+    });
+});
