805a0e576f6d5d33f2e9f9a8f1c1cefb54abfb47
Marissa Tamaki <tamaki.marissa@gmail.com>
2016-04-06 00:14:02 -0700

    Took out the code for the Animate modal

--- js/script.js
+++ js/script.js
@@ -17,53 +17,9 @@ $(document).ready(function() {
         $('body').addClass('loaded');
     }, 3000);
 
-    $("#meetup-about").animatedModal({
-        animatedIn:'zoomIn',
-        animatedOut:'bounceOut',
-        color:'#39BEB9', // where we get the background color of the Modal
-        //beforeOpen: function() {
-        //
-        //    var children = $(".thumb");
-        //    var index = 0;
-        //
-        //    function addClassNextChild() {
-        //        if (index == children.length) return;
-        //        children.eq(index++).show().velocity("transition.expandIn", { opacity:1, stagger: 250 });
-        //        window.setTimeout(addClassNextChild, 200);
-        //    }
-        //
-        //    addClassNextChild();
-        //
-        //},
-        //afterClose: function() {
-        //    $(".thumb").hide();
-        //
-        //}
-    });
 });
 
-
-//jQuery(document).ready(function($) {
-//        // Call Gridder
-//        $('.gridder').gridderExpander({
-//            scroll: true,
-//            scrollOffset: 30,
-//            scrollTo: "panel",                  // panel or listitem
-//            animationSpeed: 400,
-//            animationEasing: "easeInOutExpo",
-//            showNav: true,                      // Show Navigation
-//            //nextText: "Next",                   // Next button text
-//            //prevText: "Previous",               // Previous button text
-//            //closeText: "Close",                 // Close button text
-//            onStart: function(){
-//                console.log("Gridder Initialized");
-//            },
-//            onContent: function(){
-//                //Gridder Content Loaded
-//            },
-//            onClosed: function(){
-//                //Gridder Closed
-//            }
-//        });
-//
-//    });
+$(document).ready(function() {
+    $("#meetup-about").animatedModal({
+    });
+});
